918d26ce867788476ae8424332d0363c412ff5a3
eugunecik <zenasencuk2@gmail.com>
2025-02-19 14:55:37 +0200

    init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,321 @@
+export const users=
+    [
+        {
+          "_id": "67b48dc4f095e395083f50ac",
+          "index": 0,
+          "guid": "865dff0b-d78f-42e9-9259-6a861712398e",
+          "isActive": true,
+          "balance": "$1,869.35",
+          "picture": "http://placehold.it/32x32",
+          "age": 21,
+          "eyeColor": "blue",
+          "name": "Henrietta Franklin",
+          "gender": "female",
+          "company": "SKINSERVE",
+          "email": "henriettafranklin@skinserve.com",
+          "phone": "+1 (818) 462-3229",
+          "address": "909 Moore Place, Staples, Connecticut, 1183",
+          "about": "Eiusmod laboris et elit fugiat cupidatat id commodo cillum. Duis enim adipisicing consectetur sunt irure et est. Anim nostrud ad ullamco eiusmod ut. Laboris irure sunt aute et. Cupidatat cillum ea mollit aliquip aliqua consectetur reprehenderit consectetur excepteur.\r\n",
+          "registered": "2019-08-14T12:56:27 -03:00",
+          "latitude": 31.066683,
+          "longitude": -19.062852,
+          "tags": [
+            "cillum",
+            "amet",
+            "labore",
+            "sunt",
+            "excepteur",
+            "consectetur",
+            "veniam"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Nichols Martin"
+            },
+            {
+              "id": 1,
+              "name": "Rosalind Kline"
+            },
+            {
+              "id": 2,
+              "name": "Martha Riggs"
+            }
+          ],
+          "greeting": "Hello, Henrietta Franklin! You have 3 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "67b48dc45936061bc8a9d360",
+          "index": 1,
+          "guid": "257021d9-c01e-46a4-b09c-dda426a0d787",
+          "isActive": false,
+          "balance": "$2,670.98",
+          "picture": "http://placehold.it/32x32",
+          "age": 30,
+          "eyeColor": "blue",
+          "name": "Gibson Mcintyre",
+          "gender": "male",
+          "company": "CALCULA",
+          "email": "gibsonmcintyre@calcula.com",
+          "phone": "+1 (832) 461-2648",
+          "address": "915 Christopher Avenue, Chaparrito, Maryland, 674",
+          "about": "Ad Lorem excepteur laborum occaecat adipisicing tempor non id quis eu id sunt. Reprehenderit aliquip eiusmod sunt amet qui sint elit veniam officia consectetur cillum commodo in magna. Aliquip irure laborum nisi nisi sint nulla cupidatat nostrud sit laborum qui consectetur ea. Cupidatat non irure irure cupidatat adipisicing enim laboris anim irure sit dolore officia sit sunt. Consectetur sunt aute ipsum dolor magna ipsum aliquip adipisicing sint pariatur. Excepteur cillum elit qui sunt cupidatat occaecat sit.\r\n",
+          "registered": "2014-07-14T04:52:16 -03:00",
+          "latitude": 61.829043,
+          "longitude": 172.11238,
+          "tags": [
+            "nulla",
+            "officia",
+            "excepteur",
+            "duis",
+            "sint",
+            "eu",
+            "reprehenderit"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Schmidt Valentine"
+            },
+            {
+              "id": 1,
+              "name": "Sonia Farrell"
+            },
+            {
+              "id": 2,
+              "name": "Vickie Terrell"
+            }
+          ],
+          "greeting": "Hello, Gibson Mcintyre! You have 8 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "67b48dc421d15917622a07a0",
+          "index": 2,
+          "guid": "3a21e8eb-4a7c-4cc9-bbf3-33322715e9c9",
+          "isActive": false,
+          "balance": "$2,800.49",
+          "picture": "http://placehold.it/32x32",
+          "age": 31,
+          "eyeColor": "green",
+          "name": "Neva Joyner",
+          "gender": "female",
+          "company": "MIRACLIS",
+          "email": "nevajoyner@miraclis.com",
+          "phone": "+1 (863) 464-3553",
+          "address": "686 Hart Place, Lithium, Texas, 257",
+          "about": "Laborum commodo deserunt aliqua elit. Excepteur nisi pariatur proident elit dolore culpa ut reprehenderit incididunt occaecat mollit. Ea ut voluptate id dolore amet.\r\n",
+          "registered": "2022-02-06T12:15:26 -02:00",
+          "latitude": 86.979668,
+          "longitude": -27.836902,
+          "tags": [
+            "minim",
+            "qui",
+            "aliqua",
+            "esse",
+            "ad",
+            "reprehenderit",
+            "et"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Alvarado Lancaster"
+            },
+            {
+              "id": 1,
+              "name": "Carpenter Beck"
+            },
+            {
+              "id": 2,
+              "name": "Kim Petty"
+            }
+          ],
+          "greeting": "Hello, Neva Joyner! You have 10 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "67b48dc4cc1d37eba3400185",
+          "index": 3,
+          "guid": "178de833-f32e-45be-92db-b2f09aca6cfe",
+          "isActive": true,
+          "balance": "$1,965.26",
+          "picture": "http://placehold.it/32x32",
+          "age": 34,
+          "eyeColor": "green",
+          "name": "Minnie Parsons",
+          "gender": "female",
+          "company": "DAYCORE",
+          "email": "minnieparsons@daycore.com",
+          "phone": "+1 (856) 475-3860",
+          "address": "305 Village Road, Beyerville, Puerto Rico, 1652",
+          "about": "Incididunt eu quis mollit Lorem aliquip elit elit et. Excepteur veniam minim mollit ea consequat. Sunt sunt et nulla eu.\r\n",
+          "registered": "2020-06-02T01:58:54 -03:00",
+          "latitude": -70.821287,
+          "longitude": 70.378428,
+          "tags": [
+            "occaecat",
+            "Lorem",
+            "aliqua",
+            "tempor",
+            "enim",
+            "esse",
+            "est"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Randi Mercer"
+            },
+            {
+              "id": 1,
+              "name": "Davis Owen"
+            },
+            {
+              "id": 2,
+              "name": "Imogene Rice"
+            }
+          ],
+          "greeting": "Hello, Minnie Parsons! You have 1 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "67b48dc439a67e2b4c982132",
+          "index": 4,
+          "guid": "4a05f6b2-4afc-4901-a967-8a14be8ac055",
+          "isActive": false,
+          "balance": "$1,475.87",
+          "picture": "http://placehold.it/32x32",
+          "age": 31,
+          "eyeColor": "blue",
+          "name": "Schroeder Meyer",
+          "gender": "male",
+          "company": "ANACHO",
+          "email": "schroedermeyer@anacho.com",
+          "phone": "+1 (821) 411-2170",
+          "address": "297 Hoyts Lane, Gibsonia, Virginia, 4381",
+          "about": "Cillum ullamco minim duis culpa laboris et. Tempor excepteur in ullamco nulla sit anim cillum qui velit dolore aliqua commodo. Incididunt occaecat voluptate adipisicing ad nostrud. Quis pariatur culpa in irure labore Lorem eiusmod sit eu. Cupidatat ex ex proident cupidatat proident enim eiusmod. Id non quis ullamco esse excepteur culpa quis est incididunt. Aliquip ullamco labore exercitation esse.\r\n",
+          "registered": "2024-08-07T11:03:03 -03:00",
+          "latitude": -87.904324,
+          "longitude": 172.036931,
+          "tags": [
+            "aliquip",
+            "labore",
+            "do",
+            "deserunt",
+            "ea",
+            "excepteur",
+            "deserunt"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Ladonna Harrell"
+            },
+            {
+              "id": 1,
+              "name": "Shari Clark"
+            },
+            {
+              "id": 2,
+              "name": "Kelley Blackburn"
+            }
+          ],
+          "greeting": "Hello, Schroeder Meyer! You have 3 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "67b48dc4e7a8ec8dde2dde70",
+          "index": 5,
+          "guid": "1062eb65-26ca-4e77-8d68-fa2b8404d1f9",
+          "isActive": true,
+          "balance": "$2,134.48",
+          "picture": "http://placehold.it/32x32",
+          "age": 36,
+          "eyeColor": "green",
+          "name": "Latisha Mcguire",
+          "gender": "female",
+          "company": "FIREWAX",
+          "email": "latishamcguire@firewax.com",
+          "phone": "+1 (918) 434-2702",
+          "address": "877 McKibben Street, Whipholt, Oklahoma, 6180",
+          "about": "Commodo esse dolor mollit duis mollit adipisicing qui incididunt tempor reprehenderit. Labore elit ea dolore amet Lorem et ad. Velit nulla nulla velit dolore proident ea anim irure irure exercitation velit velit sunt Lorem. Culpa Lorem in culpa cillum quis ex amet cillum. Ipsum elit excepteur eiusmod cillum non.\r\n",
+          "registered": "2017-05-24T12:45:56 -03:00",
+          "latitude": -71.61344,
+          "longitude": -106.819667,
+          "tags": [
+            "excepteur",
+            "commodo",
+            "occaecat",
+            "aliqua",
+            "dolore",
+            "sint",
+            "deserunt"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Myrna Davis"
+            },
+            {
+              "id": 1,
+              "name": "Christa Koch"
+            },
+            {
+              "id": 2,
+              "name": "Foster Knapp"
+            }
+          ],
+          "greeting": "Hello, Latisha Mcguire! You have 3 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "67b48dc4ce31481c5730b158",
+          "index": 6,
+          "guid": "0748a87b-6184-4820-bb29-b35b8a87ef47",
+          "isActive": true,
+          "balance": "$2,943.58",
+          "picture": "http://placehold.it/32x32",
+          "age": 33,
+          "eyeColor": "brown",
+          "name": "Bates Brady",
+          "gender": "male",
+          "company": "PAPRIKUT",
+          "email": "batesbrady@paprikut.com",
+          "phone": "+1 (900) 461-2031",
+          "address": "308 Schenck Avenue, Moscow, Nevada, 5717",
+          "about": "Culpa voluptate excepteur veniam sunt. Velit consectetur mollit quis cupidatat veniam tempor. Dolore est officia duis velit deserunt laborum aliqua exercitation incididunt quis aliquip proident. Est anim nostrud labore consequat in officia incididunt minim. Occaecat quis et qui pariatur excepteur do qui ullamco minim. Id sit aute exercitation in ad veniam in.\r\n",
+          "registered": "2024-02-19T08:38:28 -02:00",
+          "latitude": 39.451042,
+          "longitude": 67.634491,
+          "tags": [
+            "occaecat",
+            "anim",
+            "laborum",
+            "aliqua",
+            "minim",
+            "duis",
+            "aliqua"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "James Sandoval"
+            },
+            {
+              "id": 1,
+              "name": "Maria Morton"
+            },
+            {
+              "id": 2,
+              "name": "Marylou William"
+            }
+          ],
+          "greeting": "Hello, Bates Brady! You have 5 unread messages.",
+          "favoriteFruit": "apple"
+        }
+      ]
+
+
+
